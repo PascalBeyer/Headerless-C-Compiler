00ff46b673c4421cd03d42332f77d19f68a20642
Pascal Beyer <>
2024-10-18 00:50:17 +0200

    Add Microsoft specific constants to limits.h

--- implicit/include/limits.h
+++ implicit/include/limits.h
@@ -22,6 +22,21 @@
 #define LONG_MAX INT_MAX
 #define ULONG_MAX UINT_MAX
 
+// Microsoft specific
+//   see https://learn.microsoft.com/en-us/cpp/c-runtime-library/data-type-constants?view=msvc-170
+#define _I8_MAX   127i8
+#define _I8_MIN (-127i8-1)
 
+#define _I16_MAX   32767i16
+#define _I16_MIN (-32767i16-1)
 
+#define _I32_MAX   2147483647i32
+#define _I32_MIN (-2147483647i32-1)
 
+#define _I64_MAX   9223372036854775807
+#define _I64_MIN (-9223372036854775807-1)
+
+#define _UI8_MAX  0xffui8
+#define _UI16_MAX 0xffffui16
+#define _UI32_MAX 0xffffffffui32
+#define _UI64_MAX 0xffffffffffffffffui64
