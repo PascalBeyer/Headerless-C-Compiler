8c1ca9b0fc239aa224966373d862c1698a62f6b2
Pascal Beyer <>
2024-10-19 10:43:36 +0200

    Commit test for redeclaring variables in the same scope.

---
+++ tests/run/redeclaring_in_same_scope_and_regrowing_the_declaration_table.c
@@ -0,0 +1,20 @@
+// run
+
+int main(){
+    int a = 1;
+    int a = 2;
+    
+    if(a != 2) return 1;
+    
+    
+    int c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    
+    if(a != 2) return 1;
+    
+    int c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z;
+    
+    if(a != 2) return 1;
+    
+    
+    return 0;
+}
