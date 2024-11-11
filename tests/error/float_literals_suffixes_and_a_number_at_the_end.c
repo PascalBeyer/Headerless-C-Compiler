76dc4008a9658c8fea8b6073a7a5cf9d89114be0
Pascal Beyer <>
2024-11-11 10:51:00 +0100

    Fix test-case commit failure.

---
+++ tests/error/float_literals_suffixes_and_a_number_at_the_end.c
@@ -0,0 +1,8 @@
+// fail
+
+// this gets identified as a float because of the e in the thing, and then the num_member_2 is postfix for the lexer
+// the parser thinks there is no suffix. Parser code needs a lot of help anyway.
+float asd = 2674151357484275986enum_member_2;
+
+int main(){}
+
