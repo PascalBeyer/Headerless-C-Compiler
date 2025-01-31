32a2c34297048b14b7025f7973603fc921c971f0
Pascal Beyer <>
2025-01-31 23:59:43 +0100

    include <malloc.h> in .mmintrin.h for some strage reason.

--- implicit/include/xmmintrin.h
+++ implicit/include/xmmintrin.h
@@ -2,3 +2,4 @@
 #define _MM_SHUFFLE(z, y, x, w) (((z) << 6) | ((y) << 4) | ((x) << 2) | (w))
 
 #include <mmintrin.h>
+#include <malloc.h> // For whatever reason, msvc includes malloc.h in xmmintrin.
